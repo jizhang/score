8eb89a455eb46f6070fc1b836ecb61f8d8157afc
Ji ZHANG <zhangji87@gmail.com>
2020-05-14 09:19:45 +0800

    add navigation

---
+++ Score/ScoreViewController.h
@@ -0,0 +1,19 @@
+//
+//  ScoreViewController.h
+//  Score
+//
+//  Created by Ji ZHANG on 2020/5/14.
+//  Copyright © 2020 Ji ZHANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ScoreViewController : UIViewController
+
+@property (strong, nonatomic) NSArray *scores;
+
+@end
+
+NS_ASSUME_NONNULL_END
